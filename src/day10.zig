652ab6341f9d1b5339d8651c59c48a4d4e52fc6e
Ben Frengley <ben.frengley@gmail.com>
2022-12-12 00:17:54 +1300

    Day 10

--- src/day10.zig
+++ src/day10.zig
@@ -12,7 +12,77 @@ const stdout = std.io.getStdOut().writer();
 
 const data = @embedFile("data/day10.txt");
 
-pub fn main() !void {}
+pub fn main() !void {
+    try stdout.print("Part 1: {}\n", .{try part1(data)});
+}
+
+fn part1(input: []const u8) !i32 {
+    var lines = util.lines(input);
+    var cpu = CPU{};
+
+    while (lines.next()) |line| {
+        const instr = try Instruction.parse(line);
+        cpu.runInstr(instr);
+    }
+
+    return cpu.signal_strength_sum;
+}
+
+const Opcode = enum {
+    noop,
+    addx,
+};
+const Instruction = union(Opcode) {
+    noop: void,
+    addx: i32,
+
+    fn parse(raw: []const u8) !Instruction {
+        var parts = tokenize(u8, raw, " ");
+        return switch (parts.next().?[0]) {
+            'n' => Instruction.noop,
+            'a' => Instruction{ .addx = try parseInt(i32, parts.next().?, 10) },
+            else => unreachable,
+        };
+    }
+};
+
+const CPU = struct {
+    cycle: i32 = 0,
+    x: i32 = 1,
+    signal_strength_sum: i32 = 0,
+
+    fn runInstr(self: *CPU, instr: Instruction) void {
+        switch (instr) {
+            Opcode.noop => self.tick(),
+            Opcode.addx => |v| {
+                self.tick();
+                self.tick();
+                self.x += v;
+            },
+        }
+    }
+
+    fn tick(self: *CPU) void {
+        self.cycle += 1;
+        switch (self.cycle) {
+            20, 60, 100, 140, 180, 220 => self.signal_strength_sum += self.cycle * self.x,
+            else => {},
+        }
+
+        // draw the sprite
+        print("{c}", .{@as(u8, if (util.abs(@mod(self.cycle - 1, 40) - self.x) <= 1) '#' else '.')});
+        if (@mod(self.cycle, 40) == 0) {
+            print("\n", .{});
+        }
+    }
+};
+
+const test_input = @embedFile("data/day10_test.txt");
+
+test "day 10 part 1" {
+    print("\n", .{});
+    try t.expectEqual(@as(i32, 13140), try part1(test_input));
+}
 
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
